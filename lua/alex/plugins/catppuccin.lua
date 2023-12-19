8351ea1be658d35ad5c2981abf716b7f828056df
alex <alex@Alex>
2023-12-19 14:33:43 -0800

    override theme colors

--- lua/alex/plugins/catppuccin.lua
+++ lua/alex/plugins/catppuccin.lua
@@ -1,4 +1,3 @@
-vim.cmd.colorscheme "catppuccin"
 vim.opt.guicursor =  "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,a:Cursor/Cursor"
 
 require("catppuccin").setup({
@@ -32,7 +31,36 @@ require("catppuccin").setup({
         types = {},
         operators = {},
     },
-    color_overrides = {},
+		color_overrides = {
+				mocha = {
+					rosewater = "#efc9c2",
+					flamingo = "#ebb2b2",
+					pink = "#f2a7de",
+					mauve = "#b889f4",
+					red = "#ea7183",
+					maroon = "#ea838c",
+					peach = "#f39967",
+					yellow = "#eaca89",
+					green = "#96d382",
+					teal = "#78cec1",
+					sky = "#91d7e3",
+					sapphire = "#68bae0",
+					blue = "#739df2",
+					lavender = "#a0a8f6",
+					text = "#b5c1f1",
+					subtext1 = "#a6b0d8",
+					subtext0 = "#959ec2",
+					overlay2 = "#848cad",
+					overlay1 = "#717997",
+					overlay0 = "#63677f",
+					surface2 = "#505469",
+					surface1 = "#3e4255",
+					surface0 = "#2c2f40",
+					base = "#1a1c2a",
+					mantle = "#141620",
+					crust = "#0e0f16",
+				},
+		},
     custom_highlights = {},
     integrations = {
         cmp = true,
@@ -46,3 +74,5 @@ require("catppuccin").setup({
 				}
     },
 })
+
+vim.cmd.colorscheme "catppuccin-mocha"
