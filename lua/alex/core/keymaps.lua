f99a21ec14343fd02d847bf2113ab911f1b79a8d
alex <alex@Alex>
2023-12-24 11:47:06 -0800

    clean up keymaps

--- lua/alex/core/keymaps.lua
+++ lua/alex/core/keymaps.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
+-- Misc
 keymap.set("n", "<leader>hh", ":noh<CR>") -- hide higlights
 keymap.set("n", "<leader>tj", ":sp | ter<CR>") -- space + t + j : split horizontally and open termianl in a new window 
 keymap.set("n", "<leader>tl", ":vs | ter<CR>") -- space + t + l : split horizontally and opern terminal in a new window
@@ -13,22 +14,19 @@ keymap.set("n", "<leader>th", ":tabnew ~/.config/nvim/lua/alex/cheatsheet.md<CR>
 keymap.set("n", "<leader>ee", ":Lex<CR>:vertical resize 50<CR>") -- open/close file explorer
 keymap.set("n", "<leader>ee", ":NvimTreeToggle<CR>:vertical resize 50<CR>")
 
-keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
-keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
-keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
-keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
-
 keymap.set("t", "<C-q>q", [[<C-\><C-n>]]) -- exit terminal mode
 keymap.set("n", "<leader>cd", "<PageDown><C-u>M", { noremap = true, silent = true })
 
 keymap.set("n", "j", "jzz", { noremap = true, silent = true })
 keymap.set("n", "k", "kzz", { noremap = true, silent = true })
 
-keymap.set('n', '<leader>fc', function() require('telescope.builtin').current_buffer_fuzzy_find() end, { desc = '[/] Fuzzily search in current buffer' })
+keymap.set("n", "<leader>wl", ":vertical resize +30<CR>")
+keymap.set("n", "<leader>wh", ":vertical resize -30<CR>")
+keymap.set("n", "<leader>we", ":wincmd =<CR>")
 
-keymap.set("n", "<leader>goj", ":GoTagAdd json<CR>")
-keymap.set("n", "<leader>goe", ":GoIfErr<CR>")
+keymap.set("n", "<leader>ww", ":set wrap<CR>")
 
+-- Harpoon
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
@@ -41,6 +39,7 @@ keymap.set("n", "<leader>h1", function() ui.nav_file(1) end)
 keymap.set("n", "<leader>h2", function() ui.nav_file(2) end)
 keymap.set("n", "<leader>h3", function() ui.nav_file(3) end)
 
+-- LLM plugin
 keymap.set("v", "<leader>lec", ":Ollama<CR>2<CR>") -- Explain code
 keymap.set("v", "<leader>lem", ":Ollama<CR>4<CR>") -- Modify code
 keymap.set("n", "<leader>lsu", ":OllamaServe<CR>") -- Ollama start 
@@ -49,11 +48,13 @@ keymap.set("n", "<leader>lsm", ":OllamaModel<CR>") -- Change model
 keymap.set("n", "<leader>ll", ":Ollama<CR>") -- List prompts
 keymap.set("v", "<leader>ll", ":Ollama<CR>") -- List prompts
 
+-- Theme switch
 keymap.set("n", "<leader>clt", ":colorscheme catppuccin-latte<CR>")
 keymap.set("n", "<leader>cdt", ":colorscheme catppuccin-mocha<CR>")
 keymap.set("n", "<leader>cgv", ":colorscheme gruvbox-material<CR>")
 keymap.set("n", "<leader>cod", ":colorscheme onedark<CR>")
 
+-- GO debugger
 local dap = require("dap")
 local widgets = require("dap.ui.widgets");
 local sidebar = widgets.sidebar(widgets.scopes);
@@ -68,12 +69,6 @@ keymap.set({ "n", "v"}, "<leader>dh", function() widgets.hover() end)
 keymap.set({"n", "v"}, "<leader>dp", function() widgets.preview() end)
 keymap.set("n", "<leader>df", function() widgets.centered_float(widgets.frames) end)
 keymap.set("n", "<leader>ds", function() widgets.centered_float(widgets.scopes) end)
-
-keymap.set("n", "<leader>wl", ":vertical resize +30<CR>")
-keymap.set("n", "<leader>wh", ":vertical resize -30<CR>")
-keymap.set("n", "<leader>we", ":wincmd =<CR>")
-
-keymap.set("n", "<leader>ww", ":set wrap<CR>")
 
 -- Go bindings
 keymap.set("n", "<leader>ir", ":GoIfErr<CR>")
--- lua/alex/plugins/telescope.lua
+++ lua/alex/plugins/telescope.lua
@@ -13,3 +13,10 @@ map('n', '<leader>ff', '<cmd>Telescope find_files<CR>', opts)
 map('n', '<leader>fs', '<cmd>Telescope live_grep<CR>', opts)
 map('n', '<leader>fb', '<cmd>Telescope buffers<CR>', opts)
 map('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', opts)
+map("n", "<leader>gc", "<cmd>Telescope git_commits<cr>", opts) -- list all git commits (use <cr> to checkout) ["gc" for git commits]
+map("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>", opts) -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
+map("n", "<leader>gb", "<cmd>Telescope git_branches<cr>", opts) -- list git branches (use <cr> to checkout) ["gb" for git branch]
+map("n", "<leader>gs", "<cmd>Telescope git_status<cr>", opts) -- list current changes per file with diff preview ["gs" for git status]
+map("n", "<leader>gt", "<cmd>Telescope git_stash<cr>", opts)
+
+vim.keymap.set('n', '<leader>fc', function() require('telescope.builtin').current_buffer_fuzzy_find() end, { desc = '[/] Fuzzily search in current buffer' })
